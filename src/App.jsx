88b0f41d2dfb6d50bb9f09d5b3c897c13d7d600a
Beyza Keser <beyzaaakeser@gmail.com>
2024-09-16 16:15:58 +0300

    TextContainer  components added

--- src/App.jsx
+++ src/App.jsx
@@ -12,17 +12,7 @@ const App = () => {
 
       <LanguageSelect/>
 
-        <div className="flex gap-3 mt-5 md:gap-[105px] max-md:flex-col">
-          <div className="flex-1">
-            <textarea className="w-full min-h-[300px] max-h-[500px] text-black text-[20px] rounded p-[10px]"></textarea>
-          </div>
-          <div>
-            <textarea
-              disabled
-              className="w-full min-h-[300px] max-h-[500px] text-gray-300 text-[20px] rounded p-[10px]"
-            ></textarea>
-          </div>
-        </div>
+        
 
         <button
           className="bg-zinc-700 px-5 py-3 rounded-md 
