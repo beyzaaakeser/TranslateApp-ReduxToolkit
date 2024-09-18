702ca790c18db65421f48ca1f9a6a5f5fa16774a
Beyza Keser <beyzaaakeser@gmail.com>
2024-09-18 13:29:25 +0300

    translateSlice extrareducers  added

--- src/redux/slices/translateSlice.js
+++ src/redux/slices/translateSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { translateText } from '../actions';
 
 const initialState = {
   isLoading: true,
@@ -10,6 +11,20 @@ const translateSlice = createSlice({
   name: 'translate',
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(translateText.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(translateText.fulfilled, (state, { payload }) => {
+      state.isLoading = false;
+      state.error = null;
+      state.answer = payload;
+    });
+    builder.addCase(translateText.rejected, (state, { error }) => {
+      state.isLoading = false;
+      state.error = error.message;
+    });
+  },
 });
 
 export default translateSlice.reducer;
